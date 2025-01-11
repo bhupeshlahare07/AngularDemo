3f2dea065ac1ff392c5e41d0030dda5740dd7eaa
bhupeshlahare <bhupeshlahare@gmail.com>
2025-01-11 19:55:59 +0530

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: AdminComponent
+    },
+    {
+        path: 'user',
+        component: UserComponent
+    }
+];
