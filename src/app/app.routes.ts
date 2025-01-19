7a95c72065eff229e0e95e44980c6fb89c972160
bhupeshlahare <bhupeshlahare@gmail.com>
2025-01-19 23:33:54 +0530

    interpolation data binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { UserComponent } from './components/user/user.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'user',
         component: UserComponent
+    },
+    {
+        path: 'data-binding',
+        component: DataBindingComponent
     }
 ];
---
+++ src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+  name : string = 'Angular';
+  currentYear : number = new Date().getFullYear();
+  currentMonth : number = new Date().getMonth();
+  currentDay : number = new Date().getDay();
+}
