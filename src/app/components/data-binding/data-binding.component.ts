96fc16963200a2b6c51e672849d6b4dbc9249914
bhupeshlahare <bhupeshlahare@gmail.com>
2025-01-20 01:21:03 +0530

    Event Binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -8,12 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  // String interpolation  
   name : string = 'Angular';
   currentYear : number = new Date().getFullYear();
   currentMonth : number = new Date().getMonth();
   currentDay : number = new Date().getDay();
 
+  // Property binding
   buttonDisabled = true;
   imageUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
   inputPlaceholder = 'Enter your name';
+
+  // Event binding
+  message = '';
+
+  onButtonClick() {
+    this.message = 'Button clicked!';
+  }
+
+  onKeyUp(event: any) {
+    this.message = event.target.value;
+  }
+
+  showMessage() {
+    alert('Hello, ' + this.message);
+  }
+
+  // Two-way data binding
 }
