de69a6b84f195125d225c319ae85124fa9f42d98
bhupeshlahare <bhupeshlahare@gmail.com>
2025-03-11 01:24:54 +0530

    control flow update

--- src/app/components/control-flow/control-flow.component.ts
+++ src/app/components/control-flow/control-flow.component.ts
@@ -13,6 +13,16 @@ export class ControlFlowComponent {
   isDiv1Visible: boolean = true;
   isDiv2Visible: string = "No";
   dayName: string = "";
+  cityList: string[] = ["New York", "Chicago", "Houston", "San Francisco", "Los Angeles"];
+  studentList: any[]=[
+    {studentId: 101, studentName: 'Sam', studentMobile:'9876543',studentCity:'Pune',isAcvive:false},
+    {studentId: 102, studentName: 'John', studentMobile:'9876543',studentCity:'Mumbai',isAcvive:true},
+    {studentId: 103, studentName: 'Smith', studentMobile:'9876543',studentCity:'Nagpur',isAcvive:false},
+    {studentId: 104, studentName: 'Tom', studentMobile:'9876543',studentCity:'Raipur',isAcvive:true},
+    {studentId: 105, studentName: 'Jerry', studentMobile:'9876543',studentCity:'Durg',isAcvive:false},
+    {studentId: 106, studentName: 'Tim', studentMobile:'9876543',studentCity:'Amravati',isAcvive:true},
+    {studentId: 107, studentName: 'Kim', studentMobile:'9876543',studentCity:'Delhi',isAcvive:true}
+  ]
 
   showHideDiv1(isShow: boolean) {
     debugger;
